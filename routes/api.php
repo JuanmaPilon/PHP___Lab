54fe4f4d0e10307d8dccde7b3f5a448ddca120c9
Juanma <juanmartin.pilon@gmail.com>
2024-05-23 15:19:04 -0300

    Ahora si quedaron bien las migration, queda hacer de nuevo lo otro con blade

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\adminController;
+
+Route::get('admin',[adminController::class, 'index']);
 
 Route::get('/comerciosyservicios', function() {
     return 'Bienvenido a comercios y servicios';
