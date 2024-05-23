8a04c67694502df5c4b2efd6435373fc64a0503d
Juanma <juanmartin.pilon@gmail.com>
2024-05-23 17:34:06 -0300

    agregado los controllers, agregado las migrations, agrergado los endpoints con los metodos, maniana sigo amigo

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Admin;
 
-class adminController extends Controller
+class AdminController extends Controller
 {
     public function index() {
         return 'Entrando a comercios y servicios';
     }
+
+    //un endpoint loco aca en contrller admin nomas
 }
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,18 @@
 <?php
 
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\adminController;
+use App\Http\Controllers\AdminController;
 
-Route::get('admin',[adminController::class, 'index']);
+Route::get('admin',[AdminController::class, 'index']);
 
 Route::get('/comerciosyservicios', function() {
     return 'Bienvenido a comercios y servicios';
 });
+
+Route::get('/usuarios', [UsuarioController::class, 'list']);
+Route::post('/usuarios', [UsuarioController::class, 'store']);
+Route::put('/usuarios/{id}', [UsuarioController::class, 'update']);
+Route::get('/usuarios/{id}', [UsuarioController::class, 'show']);
+Route::delete('usuarios/{id}', [UsuarioController::class, 'destroy']);
