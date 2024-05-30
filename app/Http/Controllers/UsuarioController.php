f0ab9da4423604b694157708938efc232917499f
Carlos <carlos.santana@estudiantes.utec.edu.uy>
2024-05-30 13:33:29 -0300

    controllers agregados y primer get y post de usuarios

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -2,9 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Usuario;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UsuarioController extends Controller
 {
     //
+    public function list()
+    {
+        $usuarios = Usuario::all();
+        if ($usuarios->isEmpty()) {
+            $data = [
+                'message' => 'no hay usuarios registrados',
+                'status' => 200
+            ];
+            return response()->json($data, 200);
+        }
+        return response()->json($usuarios, 200);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'nombreUsuario' => 'required',
+            'contrasenia' => 'required',
+            'telefono' => 'required',
+            'email' => 'required|email'
+        ]);
+
+        if ($validator->fails()) {
+            $data = [
+                'message' => 'error en la validacion de los datos',
+                'errors' => $validator->errors(),
+                'status' => 400
+            ];
+            return response()->json($data, 400);
+        }
+        $usuario = Usuario::create([
+            'nombreUsuario' => $request->nombreUsuario,
+            'contrasenia' => $request->contrasenia,
+            'telefono' => $request->telefono,
+            'email' => $request->email
+        ]);
+
+        if (!$usuario) {
+            $data = [
+                'message' => 'Error al crear el usuario',
+                'status' => 500
+            ];
+            return response()->json($data, 500);
+        }
+        $data  =[
+            'usuario' => $usuario,
+            'status' =>201 
+        ];
+        return response()->json($data, 201);
+    }
+
 }
+
+
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\UsuarioController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 
+ 
 Route::get('admin',[AdminController::class, 'index']);
 
 Route::get('/comerciosyservicios', function() {
